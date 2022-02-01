827c1861a3da502efb7262df8241694dfb09b164
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-02-01 19:47:40 +0200

    new netify address

--- src/main/java/sitowise/registry/CompanyRepository.java
+++ src/main/java/sitowise/registry/CompanyRepository.java
@@ -3,7 +3,7 @@ package sitowise.registry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("https://optimistic-hawking-fc10c8.netlify.app")
+@CrossOrigin("https://xenodochial-kepler-957fa3.netlify.app/")
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     
 }
--- src/main/java/sitowise/registry/CustomerRepository.java
+++ src/main/java/sitowise/registry/CustomerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@CrossOrigin("https://optimistic-hawking-fc10c8.netlify.app")
+@CrossOrigin("https://xenodochial-kepler-957fa3.netlify.app/")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findByCompanyIdOrderByLastNameAscFirstNameAsc(Long id);
     
